961509af3e9579ca93597dca4b44c587a9d157ab
tsmereka <tsmereka@uwmalumni.com>
2017-03-12 18:54:01 -0500

    Adding untested configuration files

---
+++ TwitchBot/ConfigurableMessageSender.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+
+namespace TwitchBot
+{
+    internal class ConfigurableMessageSender
+    {
+        public ConfigurableMessageSender(Connection connection, int messageIntervalInSeconds, List<string> configuredMessages)
+        {
+            _connection = connection;
+            _messageIntervalInSeconds = messageIntervalInSeconds;
+            _configuredMessages = configuredMessages;
+        }
+
+        private Connection _connection;
+        private int _messageIntervalInSeconds;
+        private List<string> _configuredMessages;
+
+        public void Start()
+        {
+            if (_messageIntervalInSeconds < 0 || _configuredMessages == null || _configuredMessages.Count <= 0)
+            {
+                return;
+            }
+
+            int messageIntervalInMillis = _messageIntervalInSeconds * 1000;
+            int currentIndex = 0;
+            while (_connection != null)
+            {
+                string nextMessage = _configuredMessages[currentIndex];
+                ++currentIndex;
+                currentIndex %= _configuredMessages.Count;
+
+                if (string.IsNullOrEmpty(nextMessage))
+                {
+                    continue;
+                }
+
+                // Send with lowest priority, no big deal if we're late
+                lock (this)
+                {
+                    if ( _connection != null)
+                    {
+                        _connection.Send(nextMessage);
+                    }
+                }
+                Thread.Sleep(messageIntervalInMillis);
+            }
+        }
+
+        public void Disconnect()
+        {
+            lock (this)
+            {
+                _connection = null;
+            }
+        }
+    }
+}
---
+++ TwitchBot/ConfigurationReader.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace TwitchBot
+{
+    internal class ConfigurationReader
+    {
+        public ConfigurationReader()
+        {
+            try
+            {
+                configFileStream = File.OpenRead(STANDARD_CONFIG_FILE_PATH);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public ConfigurationReader(string customPath)
+        {
+            try
+            {
+                configFileStream = File.OpenRead(customPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private const string STANDARD_CONFIG_FILE_PATH = "config\\config.xml";
+        private const int DEFAULT_MESSAGE_INTERVAL = 120;
+        private const int MINIMUM_MESSAGE_INTERVAL = 30;
+
+        private FileStream configFileStream = null;
+        private List<string> configuredMessages = null;
+        private int? configuredMessageInterval = null;
+
+        public List<string> GetConfiguredMessages()
+        {
+            if (configuredMessages == null)
+            {
+                ReadConfig();
+            }
+            return configuredMessages;
+        }
+
+        public int GetConfiguredMessageIntervalInSeconds()
+        {
+            if(configuredMessageInterval == null)
+            {
+                ReadConfig();
+            }
+            return configuredMessageInterval ?? DEFAULT_MESSAGE_INTERVAL;
+        }
+
+        private void ReadConfig()
+        {
+            if (configFileStream == null)
+            {
+                return;
+            }
+
+            XmlDocument document = new XmlDocument();
+            document.Load(configFileStream);
+
+            XmlNode intervalNode = document.DocumentElement.SelectSingleNode("/config/interval");
+            XmlAttributeCollection attributes;
+            XmlNode waitTimeNode;
+            if (intervalNode != null && (attributes = intervalNode.Attributes) != null && (waitTimeNode = attributes.GetNamedItem("wait-time")) != null)
+            {
+                string value = waitTimeNode.Value;
+                configuredMessageInterval = int.Parse(value);
+                if (configuredMessageInterval < MINIMUM_MESSAGE_INTERVAL)
+                {
+                    configuredMessageInterval = MINIMUM_MESSAGE_INTERVAL;
+                }
+            }
+            else
+            {
+                configuredMessageInterval = DEFAULT_MESSAGE_INTERVAL;
+            }
+
+            XmlNodeList messagesList = document.DocumentElement.SelectNodes("/config/messages/message");
+            if (messagesList != null)
+            {
+                configuredMessages = new List<string>(messagesList.Count);
+                foreach( XmlNode messageNode in messagesList)
+                {
+                    if (messageNode != null && !string.IsNullOrEmpty(messageNode.Value))
+                    {
+                        configuredMessages.Add(messageNode.Value);
+                    }
+                }
+            }
+        }
+    }
+}
