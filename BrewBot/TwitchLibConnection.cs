b41464c71230540d2fa1566bb08e8aeda4d7bbb6
tsmereka <tsmereka@uwmalumni.com>
2017-09-04 15:46:52 -0500

    More connection work

--- BrewBot/TwitchLibConnection.cs
+++ BrewBot/TwitchLibConnection.cs
@@ -12,18 +12,21 @@ namespace BrewBot
 		/// Create a new connection
 		/// </summary>
 		/// <param name="chat">Target IRC chat</param>
-		public TwitchLibConnection( string hostname, int port, string chat, string username, string oauth, CommandFactory commandFactory, string subscriberTitle )
+		public TwitchLibConnection( string hostname, int port, string chat, string username, string oauth, string subscriberTitle, CommandFactory commandFactory, UserManager userManager )
 		{
 			_channel = chat;
 			_subscriberTitle = subscriberTitle;
 			_credentials = new ConnectionCredentials( username, oauth, hostname, port );
 			_commandFactory = commandFactory;
+			_userManager = userManager;
 
 			_client = new TwitchClient( _credentials, _channel );
-			_client.OnJoinedChannel += onJoinedChannel;
-			_client.OnMessageReceived += onMessageReceived;
-			_client.OnWhisperReceived += onWhisperReceived;
-			_client.OnNewSubscriber += onNewSubscriber;
+			_client.OnJoinedChannel += OnJoinedChannel;
+			_client.OnMessageReceived += OnMessageReceived;
+			_client.OnWhisperReceived += OnWhisperReceived;
+			_client.OnNewSubscriber += OnNewSubscriber;
+			_client.OnUserJoined += OnUserJoined;
+			_client.OnUserLeft += OnUserLeft;
 		}
 
 		private readonly string _channel;
@@ -31,17 +34,28 @@ namespace BrewBot
 		private ConnectionCredentials _credentials;
 		private TwitchClient _client;
 		private CommandFactory _commandFactory;
+		private UserManager _userManager;
 
+		/// <summary>
+		/// Connect to the Twitch server
+		/// </summary>
 		public void Connect()
 		{
 			_client.Connect();
 		}
 
+		/// <summary>
+		/// Disconnect from the twitch server
+		/// </summary>
 		public void Disconnect()
 		{
 			_client.Disconnect();
 		}
 
+		/// <summary>
+		/// Sends an IRC message.
+		/// </summary>
+		/// <param name="text"></param>
 		public void Send( string text )
 		{
 			if ( _client.IsConnected )
@@ -50,6 +64,10 @@ namespace BrewBot
 			}
 		}
 
+		/// <summary>
+		/// Sends a RAW IRC message.
+		/// </summary>
+		/// <param name="text"></param>
 		public void SendRaw( string text )
 		{
 			if ( _client.IsConnected )
@@ -58,12 +76,12 @@ namespace BrewBot
 			}
 		}
 
-		private void onJoinedChannel( object sender, OnJoinedChannelArgs e )
+		private void OnJoinedChannel( object sender, OnJoinedChannelArgs e )
 		{
 			Send( Strings.ChannelJoined );
 		}
 
-		private void onMessageReceived( object sender, OnMessageReceivedArgs e )
+		private void OnMessageReceived( object sender, OnMessageReceivedArgs e )
 		{
 			// TODO configure a list of banned words
 			if ( e.ChatMessage.Message.Contains( "badword" ) )
@@ -72,7 +90,7 @@ namespace BrewBot
 			}
 		}
 
-		private void onCommandReceived( object sender, OnWhisperCommandReceivedArgs e )
+		private void OnCommandReceived( object sender, OnWhisperCommandReceivedArgs e )
 		{
 			if ( _commandFactory != null && _client.IsConnected )
 			{
@@ -84,12 +102,12 @@ namespace BrewBot
 			}
 		}
 
-		private void onWhisperReceived( object sender, OnWhisperReceivedArgs e )
+		private void OnWhisperReceived( object sender, OnWhisperReceivedArgs e )
 		{
 			// We currently don't care about whispers
 		}
 
-		private void onNewSubscriber( object sender, OnNewSubscriberArgs e )
+		private void OnNewSubscriber( object sender, OnNewSubscriberArgs e )
 		{
 			if ( e.Subscriber.IsTwitchPrime )
 			{
@@ -101,6 +119,16 @@ namespace BrewBot
 				string message = string.Format( Strings.SubscriptionReceived, e.Subscriber.DisplayName, _subscriberTitle );
 				Send( message );
 			}
+		}
+
+		private void OnUserJoined( object sender, OnUserJoinedArgs e )
+		{
+			_userManager.LoginUser( e.Username );
+		}
+
+		private void OnUserLeft( object sender, OnUserLeftArgs e )
+		{
+			_userManager.LogoutUser( e.Username );
 		}
 	}
 }
